81198c5b8a872910f9465220766fd3f7db9e7256
Christopher Ngai <christopherngai@Christophers-MacBook-Pro-3.local>
2018-10-02 23:01:18 -0700

    add/delete note functionality

--- app/components/main.js
+++ app/components/main.js
@@ -8,8 +8,25 @@ import {
     TouchableOpacity,
 } from 'react-native';
 
+import Note from './note';
+
 export default class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteArray: [],
+      noteText: '',
+    }
+  }
+
   render() {
+
+    let notes = this.state.noteArray.map((val, key) => {
+      return <Note key={key} keyVal={key} val={val}
+              deleteMethod={ ()=> this.deleteNote(key) } />
+    });
+
     return (
       <View style={styles.container}>
 
@@ -18,13 +35,15 @@ export default class Main extends Component {
         </View>
 
         <ScrollView style={styles.scrollContainer}>
-
+          {notes}
         </ScrollView>
 
         <View style={styles.footer}>
 
           <TextInput
             style={styles.textInput}
+            onChangeText={(noteText) => this.setState({noteText})}
+            value={this.state.noteText}
             placeholder='>note'
             placeholderTextColor='white'
             underlineColorAndroid='transparent'>
@@ -33,12 +52,32 @@ export default class Main extends Component {
 
         </View>
 
-        <TouchableOpacity style={styles.addButton}>
+        <TouchableOpacity onPress={ this.addNote.bind(this) } style={styles.addButton}>
           <Text style={styles.addButtonText}>+</Text>
         </TouchableOpacity>
 
       </View>
     );
+  }
+
+  addNote() {
+
+    if (this.state.noteText) {
+      var d = new Date();
+      this.state.noteArray.push({
+        'date': d.getFullYear() +
+        "/" + (d.getMonth() + 1) +
+        "/" + d.getDate(),
+        'note': this.state.noteText
+      });
+      this.setState({ noteArray: this.state.noteArray });
+      this.setState({ noteText: ''});
+    }
+  }
+
+  deleteNote(key) {
+    this.state.noteArray.splice(key, 1);
+    this.setState({ noteArray: this.state.noteArray })
   }
 }
 
