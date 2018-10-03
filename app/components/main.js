a99fb4f8db54226f16ea0206039249358fcbdb17
Christopher Ngai <christopherngai@Christophers-MacBook-Pro-3.local>
2018-10-02 21:44:56 -0700

    added main component

---
+++ app/components/main.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    ScrollView,
+    TouchableOpacity,
+} from 'react-native';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <View><Text>Testing</Text></View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+  
+  },
+});
